bbb15c71b0051c09e69f960f5d5128064338057e
sasaanka <sasanka2629@gmail.com>
2025-05-19 15:52:46 +0530

    UC3 : Introduncing DTO and model to AddressBook app

---
+++ src/main/java/com/example/demo/dto/AddressBookDTO.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+public class AddressBookDTO {
+    private String name;
+    private String address;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
