2ac0b615f553a743f87ee655da94dbb0affdaf7a
sasaanka <sasanka2629@gmail.com>
2025-05-19 16:07:13 +0530

    UC5 : Service layer stored the AddressBook data

--- src/main/java/com/example/demo/Controller/AddressBookController.java
+++ src/main/java/com/example/demo/Controller/AddressBookController.java
@@ -1,5 +1,45 @@
 package com.example.demo.Controller;
 
+
+import com.example.demo.dto.AddressBookDTO;
+import com.example.demo.model.AddressBook;
+import com.example.demo.service.IAddressBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/addressbook")
 public class AddressBookController {
 
+    @Autowired
+    private IAddressBookService service;
+
+    @PostMapping
+    public ResponseEntity<AddressBook> add(@RequestBody AddressBookDTO dto) {
+        return ResponseEntity.ok(service.addAddress(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AddressBook>> getAll() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AddressBook> getById(@PathVariable int id) {
+        return ResponseEntity.ok(service.getById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AddressBook> update(@PathVariable int id, @RequestBody AddressBookDTO dto) {
+        return ResponseEntity.ok(service.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        service.delete(id);
+        return ResponseEntity.ok("Deleted ID: " + id);
+    }
 }
