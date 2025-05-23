80c9945720e205dd0b6059999bf1e7ec439a149f
sasaanka <sasanka2629@gmail.com>
2025-05-23 17:16:31 +0530

    UC6 : Getters and setters are generated using Lombok library

---
+++ src/main/java/com/example/demo/dto/ContactDTO.java
@@ -0,0 +1,21 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class ContactDTO {
+    public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	private String name;
+    private String email;
+}
